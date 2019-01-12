41837f0c5bc06316a41ded55526c1f0fc1821d2a
TimiTao <kunicki.tomasz@gmail.com>
2019-01-12 09:36:54 +0100

    fix

--- src/Client.php
+++ src/Client.php
@@ -1,25 +1,23 @@
 <?php
 /**
- *
  * This file is part of the Aggrego.
  * (c) Tomasz Kunicki <kunicki.tomasz@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
  */
 
 declare(strict_types = 1);
 
 namespace Aggrego\MessengerCommandConsumer;
 
-use Aggrego\CommandConsumer\Client;
+use Aggrego\CommandConsumer\Client as CommandConsumerClient;
 use Aggrego\CommandConsumer\Command;
 use Aggrego\CommandConsumer\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\HandledStamp;
 
-class MessageBusClient implements Client
+class Client implements CommandConsumerClient
 {
     /** @var MessageBusInterface */
     private $bus;
