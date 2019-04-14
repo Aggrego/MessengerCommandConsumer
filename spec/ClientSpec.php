955359faaff5c4c55c8ea6e95a9c2e9743df0b9e
TimiTao <kunicki.tomasz@gmail.com>
2019-04-14 08:38:30 +0200

    #1 update license & code

--- spec/ClientSpec.php
+++ spec/ClientSpec.php
@@ -1,18 +1,23 @@
 <?php
 /**
+ *
  * This file is part of the Aggrego.
  * (c) Tomasz Kunicki <kunicki.tomasz@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
  */
 
 declare(strict_types = 1);
 
 namespace spec\Aggrego\MessengerCommandConsumer;
 
+use Aggrego\CommandConsumer\Command;
 use Aggrego\MessengerCommandConsumer\Client;
 use PhpSpec\ObjectBehavior;
+use stdClass;
+use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 class ClientSpec extends ObjectBehavior
@@ -26,4 +31,12 @@ class ClientSpec extends ObjectBehavior
     {
         $this->shouldHaveType(Client::class);
     }
+
+    function it_should_consume(MessageBusInterface $bus, Command $command)
+    {
+
+        $bus->dispatch($command)->willReturn(new Envelope(new stdClass()));
+        $this->beConstructedWith($bus);
+        $this->consume($command)->shouldReturn(null);
+}
 }
